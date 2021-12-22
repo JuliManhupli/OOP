2ae5c0598f52223ca6cd23dc07f32a9b8a892ac5
JuliZliuka <juliamangup@gmail.com>
2021-12-22 16:46:55 +0200

    Update task.py

--- lr4/task3/task.py
+++ lr4/task3/task.py
@@ -1,12 +1,23 @@
 from task3.abstract import *
-from task3.connector_db import *
+from task3.config import *
 
 cursor = db.cursor()
 
 
 class CourseFactory(ICourseFactory):
-
+    """
+    This is a class for creating new courses and teachers
+    """
     def create_course(self, course_name, course_program, course_type, teacher):
+        """
+        Method to create a new course
+        :param course_name: name of course
+        :param course_program: course
+        :param course_type: course type (local or offsite)
+        :param teacher: name of the teacher who is teaching the course
+
+        :return: class instance LocalCourse or OffsiteCourse
+        """
         if course_type == "local":
             self.add_course(course_name, course_program, "local", teacher)
             return LocalCourse(course_name, course_program, teacher)
@@ -17,6 +28,15 @@ class CourseFactory(ICourseFactory):
             TypeError("Incorrect course type")
 
     def add_course(self, course_name, course_program, course_type, teacher):
+        """
+        Method that adds information about course to the database
+        :param course_name: name of course
+        :param course_program: course
+        :param course_type: course type (local or offsite)
+        :param teacher: name of the teacher who is teaching the course
+
+        :return: None
+        """
         id_teacher = 0
         all_teacher = self.get_teacher()
         for k, v in all_teacher.items():
@@ -29,21 +49,43 @@ class CourseFactory(ICourseFactory):
         db.commit()
 
     def get_course(self):
+        """
+        Method that returns information about all courses
+
+        :return: tuple of courses data values
+        """
         cursor.execute("SELECT course.course_name, course.course_type, course.course_program, teacher.teacher_name "
                        "FROM course JOIN teacher ON teacher.id = course.id_teacher")
         result = cursor.fetchall()
         return result
 
     def create_teacher(self, name):
+        """
+        Method for creating a new teacher
+        :param name: name of teacher
+
+        :return: class instance Teacher
+        """
         self.add_teacher(name)
         return Teacher(name)
 
     def add_teacher(self, name):
+        """
+        Method that adds information about name to the database
+        :param name: name of teacher
+
+        :return: None
+        """
         sql = "INSERT INTO teacher (teacher_name) VALUES (%s)"
         cursor.execute(sql, (name,))
         db.commit()
 
     def get_teacher(self):
+        """
+        Method that returns information about all teachers
+
+        :return: tuple of teachers data values
+        """
         cursor.execute("SELECT * FROM teacher")
         result = cursor.fetchall()
         all_teacher = dict(result)
@@ -51,17 +93,33 @@ class CourseFactory(ICourseFactory):
 
 
 class Teacher(ITeacher, ABC):
+    """
+    Class that stores information about a teacher
+    """
 
     def __init__(self, name):
+        """
+        Class constructor Teacher
+        :param name: name of teacher
+        """
         self.name = name
         self.__courses = []
 
     @property
     def name(self):
+        """
+        Teacher name getter
+        :return: name
+        """
         return self.__name
 
     @name.setter
     def name(self, name):
+        """
+        Teacher name setter
+        :param name: name
+        :return: None
+        """
         if not name:
             raise ValueError("Teacher name is not specified")
         if not isinstance(name, str):
@@ -69,27 +127,53 @@ class Teacher(ITeacher, ABC):
         self.__name = name
 
     def add_course(self, course):
+        """
+        Method to add a course to the teacher's class
+        :param course: class instance LocalCourse or OffsiteCourse
+        :return: None
+        """
         if not isinstance(course, (LocalCourse, OffsiteCourse)):
             raise TypeError("That is not a LocalCourse or OffsiteCourse type!")
         self.__courses.append(course)
 
     def __str__(self):
+        """
+        Objects in string form
+        :return: string of class value
+        """
         return f'Teacher {self.__name}'
 
 
 class Course(ICourse):
-
+    """
+    Course class
+    """
     def __init__(self, course_name, course_program, teacher):
+        """
+        Class constructor Course
+        :param course_name: name of course
+        :param course_program: course program
+        :param teacher: name of teacher
+        """
         self.course_name = course_name
         self.course_program = course_program
         self.teacher = teacher
 
     @property
     def course_name(self):
+        """
+        Course name getter
+        :return: course_name
+        """
         return self.__course_name
 
     @course_name.setter
     def course_name(self, course_name):
+        """
+        Course name setter
+        :param course_name: name of course
+        :return: None
+        """
         if not course_name:
             raise ValueError("Course name is not specified")
         if not isinstance(course_name, str):
@@ -98,10 +182,19 @@ class Course(ICourse):
 
     @property
     def course_program(self):
+        """
+        Course program getter
+        :return: course_program
+        """
         return self.__course_program
 
     @course_program.setter
     def course_program(self, course_program):
+        """
+        Course program setter
+        :param course_program: course program
+        :return: None
+        """
         if not isinstance(course_program, list):
             raise TypeError("Incorrect course program type")
         if not all(isinstance(value, str) for value in course_program):
@@ -110,39 +203,80 @@ class Course(ICourse):
 
     @property
     def teacher(self):
+        """
+        Teacher getter
+        :return: teacher
+        """
         return self.__teacher
 
     @teacher.setter
     def teacher(self, teacher):
+        """
+        Teacher setter
+        :param teacher: class instance Teacher
+        :return: None
+        """
         if not isinstance(teacher, Teacher):
             raise TypeError("That is not a Teacher type!")
         self.__teacher = teacher
 
     def __str__(self):
+        """
+        Objects in string form
+        :return: string of class value
+        """
         return f'Course: {self.__course_name}\n' \
                f'{self.__teacher}\n' \
                f'program {self.__course_program}\n'
 
 
 class LocalCourse(ILocalCourse, Course):
+    """
+    Class of Local Courses
+    Implements Course
+    """
 
     def __init__(self, course_name, course_program, teacher):
+        """
+        Class constructor LocalCourse
+        :param course_name: name of course
+        :param course_program: course
+        :param teacher: name of the teacher who is teaching the course
+        """
         super().__init__(course_name, course_program, teacher)
         self.course_type = "local"
 
     def __str__(self):
+        """
+        Objects in string form
+        :return: string of class value
+        """
         return f'Local course: {self.course_name}\n' \
                f'{self.teacher}\n' \
                f'program {self.course_program}\n'
 
 
 class OffsiteCourse(IOffsiteCourse, Course):
+    """
+    Class of Offsite Courses
+    Implements Course
+    """
 
     def __init__(self, course_name, course_program, teacher):
+        """
+        Class constructor OffsiteCourse
+        :param course_name: name of course
+        :param course_program: course
+        :param teacher: name of the teacher who is teaching the course
+        """
         super().__init__(course_name, course_program, teacher)
         self.course_type = "offsite"
 
     def __str__(self):
+        """
+        Objects in string form
+        :return: string of class value
+        """
         return f'Offsite course: {self.course_name}\n' \
                f'{self.teacher}\n' \
                f'program {self.course_program}\n'
